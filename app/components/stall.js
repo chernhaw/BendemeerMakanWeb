1a9a2a3ea21790850dd7abb63687ad91ee46e9a4
chernhaw <38425578+chernhaw@users.noreply.github.com>
2025-06-01 16:11:26 +0700

    Update stall.js
    Fix image not showing

--- app/components/stall.js
+++ app/components/stall.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import Image from "next/image";
 
-const Stall = ({image, unit, name})=>{
-    return (
-      <div>
-         < Image
-        src = {image}
+const Stall = ({ image, unit, name }) => {
+  return (
+    <div>
+      <img
+        src={`${image}`}
         alt={name}
         width={500}
         height={300}
       />
-      <div>{unit} </div>
-      <div>{name} </div>
-      </div>
-    );
+      <div>{unit}</div>
+      <div>{name}</div>
+    </div>
+  );
 };
 
 export default Stall;
